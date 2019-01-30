2b5f08b41202b0ffa945076c8fc27ba47ea44f78
dingyan <dingyan1105@163.com>
2019-01-31 05:14:56 +0800

    fix：搜索内容为空是不保存记录

--- lib/manager/search_history_manager.dart
+++ lib/manager/search_history_manager.dart
@@ -5,17 +5,19 @@ class SearchHistoryManager {
   static const String SEARCH_HISTORY_LIST = 'search_history_list';
 
   static add(String history) async {
-    SharedPreferences preferences = await PreferencesManager.getInstance();
-    List<String> historys = await findAll();
-    if (historys.contains(history)) {
-      // 如果已经保存了当前记录,将位置移动到第一个
-      historys.remove(history);
-    }
-    int index = 0;
-    historys.insert(index, history);
+    if (history.isNotEmpty) {
+      SharedPreferences preferences = await PreferencesManager.getInstance();
+      List<String> historys = await findAll();
+      if (historys.contains(history)) {
+        // 如果已经保存了当前记录,将位置移动到第一个
+        historys.remove(history);
+      }
+      int index = 0;
+      historys.insert(index, history);
 
-    // 保存记录
-    await preferences.setStringList(SEARCH_HISTORY_LIST, historys);
+      // 保存记录
+      await preferences.setStringList(SEARCH_HISTORY_LIST, historys);
+    }
   }
 
   static Future<List<String>> findAll() async {
