a616e56205ddc7a1ac7957481ef967fa2d363f68
dingyan <dingyan1105@163.com>
2019-01-31 05:13:10 +0800

    add

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -4,4 +4,16 @@ class StringValus {
   static const String RETRACT = '撤回';
   static const String EMPTY_NO_DATA_REMARK = '哎呀 没数据';
   static const String EMPTY_NO_FAVORITE_DATA_REMARK = '看着我干什么\n还不快去收藏';
+  static const String FAVORITE_ARTICLE_SUCCESS = '收藏成功';
+  static const String CANCEL_FAVORITE_ARTICLE_SUCCESS = '取消收藏成功';
+  static const String POST_GANK = '提交干货';
+  static const String TEXT_FILED_URL_HINT_TEXT = '请输入网址';
+  static const String TEXT_FILED_URL_LABEL_TEXT = '网址';
+  static const String TEXT_FIELD_URL_EMPTY_TEXT = '网址不能为空';
+  static const String TEXT_FILED_DESC_HINT_TEXT = '请输入描述';
+  static const String TEXT_FILED_DESC_LABEL_TEXT = '描述';
+  static const String TEXT_FIELD_DESC_EMPTY_TEXT = '描述不能为空';
+  static const String TEXT_FILED_NICKNAME_HINT_TEXT = '请输入昵称';
+  static const String TEXT_FILED_NICKNAME_LABEL_TEXT = '昵称';
+  static const String TEXT_FIELD_NICKNAME_EMPTY_TEXT = '昵称不能为空';
 }
