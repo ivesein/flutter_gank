e3195ed4e7f22a573defe50816835711878fe1c8
dingyan <dingyan1105@163.com>
2019-02-07 01:37:34 +0800

    add：基础数据

---
+++ lib/constant/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  // GitHub相关
+  static const CLIENT_ID = '750419fa775225670b05';
+  static const CLIENT_SECRET = '6f12853d2f72abbd13548239cf8040783e6e47df';
+  static const NOTE = 'com.lijinshanmx.gank';
+  static const NOTE_URL = 'https://github.com/lijinshanmx/gank/CallBack';
+  static const List<String> GANK_OAUTH2_SCOPE = [
+    'user',
+    'repo',
+    'gist',
+    'notifications'
+  ];
+}
