374d97bad0c8b294ad40e7dff6a62e98ae5aafc1
dingyan <dingyan1105@163.com>
2019-02-08 00:25:16 +0800

    add：移除本地用户信息

--- lib/manager/user_manager.dart
+++ lib/manager/user_manager.dart
@@ -6,17 +6,27 @@ import '../model/user_info.dart';
 class UserManager {
   static const String KEY_USER_INFO = 'key_user_info';
 
+  /// 是否登录
   static Future<bool> isLogin() async {
     SharedPreferences preferences = await PreferencesManager.getInstance();
     return preferences.getString(KEY_USER_INFO) != null;
   }
 
+  /// 保存用户信息到本地
+  /// [userInfo] 用户信息
   static Future<void> saveToLocal(UserInfo userInfo) async {
     String userInfoData = json.encode(userInfo.toJson());
     SharedPreferences preferences = await PreferencesManager.getInstance();
     await preferences.setString(KEY_USER_INFO, userInfoData);
   }
 
+  /// 从本地移除用户信息
+  static Future<bool> removeFromLocal() async {
+    SharedPreferences preferences = await PreferencesManager.getInstance();
+    return preferences.remove(KEY_USER_INFO);
+  }
+
+  /// 获取用户信息
   static Future<UserInfo> getUserInfo() async {
     SharedPreferences preferences = await PreferencesManager.getInstance();
     String userInfoData = preferences.getString(KEY_USER_INFO);
