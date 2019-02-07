8b5c8a6b762695b740c5f430e5f8653c4347b34d
dingyan <dingyan1105@163.com>
2019-02-08 00:33:59 +0800

    update：title

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './page/home_page.dart';
+import './values/strings.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Gank',
+      title: StringValus.APP_NAME,
       theme: ThemeData(
           primaryColor: Colors.blueGrey,
           primarySwatch: Colors.blue,
