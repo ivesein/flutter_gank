784a78326f8c039558baf60c6b174ac51dc4aaf8
dingyan <dingyan1105@163.com>
2019-02-09 02:08:03 +0800

    delete：资源文件

--- lib/values/images.dart
+++
@@ -1,5 +0,0 @@
-class ImageValues {
-  static const String EMPTY_VIEW_NO_DATA_IMAGE = 'images/ic_empty_no_data.gif';
-  static const String EMPTY_VIEW_NO_FAVORITE_IMAGE =
-      'images/ic_empty_no_favorite_data.gif';
-}
